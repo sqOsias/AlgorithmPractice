304d8c5f259e114fce9505a5b039a9594614e7c8
wuyc5 <145456021@qq.com>
2025-07-10 18:45:41 +0800

    0710

---
+++ fixedLengthSlipWindow/Solution2_1456.java
@@ -0,0 +1,27 @@
+package fixedLengthSlipWindow;
+
+class Solution2_1456 {
+    //定长字串中最大数目的元音
+    public int maxVowels(String s, int k) {
+        char[] charArray = s.toCharArray();
+        int ans = 0;
+        int vowel = 0;
+        for (int i=0;i<s.length();i++){
+            //1.进入窗口
+            if(charArray[ i]=='a'||charArray[ i]=='e'||charArray[ i]=='i'||charArray[ i]=='o'||charArray[ i]=='u'){
+                vowel++;
+            }
+            if (i<k-1){
+                continue;
+            }
+            //2.更新答案
+            ans = Math.max(ans,vowel);
+            //3.离开窗口
+            char out = charArray[i-k+1];
+            if(out=='a'||out=='e'||out=='i'||out=='o'||out=='u'){
+                vowel--;
+            }
+        }
+        return ans;
+    }
+}
---
+++ fixedLengthSlipWindow/Solution5_2379.java
@@ -0,0 +1,28 @@
+package fixedLengthSlipWindow;
+
+// 2379. 得到 K 个黑块的最少涂色次数
+class Solution5_2379 {
+
+    public int minimumRecolors(String blocks, int k) {
+        int res=k;
+        int num =0;
+        char[] charArray = blocks.toCharArray();
+        for(int i =0;i<charArray.length;i++){
+            if(charArray[i]=='W'){
+                num++;
+            }
+            if(i<k-1){
+                continue;
+            }
+            res=Math.min(res,num);
+            if(charArray[i-k+1]=='W'){
+                num--;
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        Solution5_2379 s = new Solution5_2379();
+        System.out.println(s.minimumRecolors("WBBWWBBWBW", 7));
+    }
+}
