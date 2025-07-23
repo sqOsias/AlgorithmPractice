597b90aebd40a494b27a9c129faf2c07759a9ad0
wuyc5 <145456021@qq.com>
2025-07-23 09:37:46 +0800

    feat(0722):2.3.1

---
+++ noFixedLengthSlipWindow/Solution17_2962.java
@@ -0,0 +1,65 @@
+package noFixedLengthSlipWindow;
+
+import java.util.HashMap;
+
+/**
+ * 给你一个整数数组 nums 和一个 正整数 k 。
+ *
+ * 请你统计有多少满足 「 nums 中的 最大 元素」至少出现 k 次的子数组，并返回满足这一条件的子数组的数目。
+ *
+ * 子数组是数组中的一个连续元素序列。
+ * 输入：nums = [1,3,2,3,3], k = 2
+ * 输出：6
+ * 解释：包含元素 3 至少 2 次的子数组为：[1,3,2,3]、[1,3,2,3,3]、[3,2,3]、[3,2,3,3]、[2,3,3] 和 [3,3]
+ */
+class Solution17_2962 {
+    public long countSubarrays(int[] nums, int k) {
+        int target =0;
+        for(int num:nums){
+            target = Math.max(target,num);
+        }
+        long res=0;
+        HashMap<Integer, Integer> cnt = new HashMap<>();
+        cnt.put(target,0);
+        for (int left = 0, right =0 ;right<nums.length;right++){
+            cnt.merge(nums[right],1,Integer::sum);
+            while(cnt.get(target)>=k){
+                cnt.merge(nums[left],-1,Integer::sum);
+                left++;
+            }
+            res += left;
+        }
+        return res;
+    }
+    // 不用map 时间缩短很多
+    public long countSubarrays2(int[] nums, int k) {
+        int mx = 0;
+        for (int x : nums) {
+            mx = Math.max(mx, x);
+        }
+
+        long ans = 0;
+        int cntMx = 0, left = 0;
+        for (int x : nums) {
+            if (x == mx) {
+                cntMx++;
+            }
+            while (cntMx == k) {
+                if (nums[left] == mx) {
+                    cntMx--;
+                }
+                left++;
+            }
+            ans += left;
+        }
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,4,2,1};
+        int k = 3;
+        Solution17_2962 s = new Solution17_2962();
+        System.out.println(s.countSubarrays(nums, k));
+    }
+}
---
+++ noFixedLengthSlipWindow/Solution18_3325.java
@@ -0,0 +1,32 @@
+package noFixedLengthSlipWindow;
+
+/**
+ * 给你一个字符串 s 和一个整数 k，在 s 的所有子字符串中，请你统计并返回 至少有一个 字符 至少出现 k 次的子字符串总数。
+ *
+ * 子字符串 是字符串中的一个连续、 非空 的字符序列。
+ * 输入： s = "abacb", k = 2
+ *
+ * 输出： 4
+ */
+class Solution18_3325 {
+    public int numberOfSubstrings(String s, int k) {
+        char[] S = s.toCharArray();
+        int n = S.length;
+        int[]cnt = new int[128];
+        int ans = 0;
+        for (int left = 0 ,right = 0; right < n; right++) {
+            cnt[S[right]]++;
+            while(cnt[S[right]] == k){
+                cnt[S[left]]--;
+                left++;
+            }
+            ans += left;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution18_3325 s = new Solution18_3325();
+        System.out.println(s.numberOfSubstrings("abcde", 1));
+    }
+}
